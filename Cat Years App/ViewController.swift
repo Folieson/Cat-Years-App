753228aa6a7442f196426332562d94578e7bd92b
Андрей Понамарчук <ponamarchuk.andrey@icloud.com>
2018-07-28 23:27:48 +0500

    Initial Commit

---
+++ Cat Years App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cat Years App
+//
+//  Created by Андрей Понамарчук on 28/07/2018.
+//  Copyright © 2018 Андрей Понамарчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
